54f10baf625f6c2d292fce9f0d0b37f94ac052cd
waseGithub <108359561+waseGithub@users.noreply.github.com>
2023-12-20 13:48:53 +0000

    Update check_script_mega.sh

--- src/feather_m4/check_script_mega.sh
+++ src/feather_m4/check_script_mega.sh
@@ -1,2 +1,16 @@
-ls
+#!/bin/bash
 
+# Name of the process to check
+PROCESS="log_from_mega.py"
+# Command to start the process
+COMMAND="python3 /home/wase/threshold-fed-reactor/src/feather_m4/log_from_mega.py"
+
+# Check if the process is running
+if pgrep -f $PROCESS > /dev/null
+then
+    echo "$PROCESS is running."
+else
+    echo "$PROCESS is not running."
+    # Start the process if it's not running
+    $COMMAND &
+fi
